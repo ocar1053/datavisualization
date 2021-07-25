139bbf874c64e8c5b684fbd0165db1420a8a3732
ocar1053 <ocar8951@gmail.com>
2021-07-26 00:24:36 +0800

    update bootstrap

--- road.js
+++ road.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-	function copyToClipboard(text, k) {
-		var dummy = document.createElement("textarea");
-		// to avoid breaking orgain page when copying more words
-		// cant copy when adding below this code
-		// dummy.style.display = 'none'
-		document.body.appendChild(dummy);
-		//Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-		dummy.value = text;
-		dummy.select();
-		document.execCommand("copy");
-		document.body.removeChild(dummy);
-	}
 	let urll = "jsonFile/rank.json";
 	let requst = new XMLHttpRequest();
 	requst.open("GET", urll); //resquest local data
@@ -21,12 +9,11 @@ $(document).ready(function () {
 
 		// to show top ten data in leftside
 		for (let k = 0; k < datarespose.length; k++) {
-			$("#listadd").append(
-				`<li  id="${k}list" class="ui-li-static ui-body-inherit" style="
-			text-align: center";>${datarespose[k].site}<button id="btn${k}" class="btn"><i class="far fa-copy"></i></button></li>`
+			$(".list-group").append(
+				`<button id="btn${k}" style="
+				text-align: center;" type="button" class="list-group-item list-group-item-action"> ${datarespose[k].site}</button>`
 			);
 
-			let listText = document.getElementById(`${k}list`);
 			$(`#btn${k}`).click(function (e) {
 				document.getElementsByClassName(
 					"leaflet-control-geocoder leaflet-bar leaflet-control"
@@ -37,8 +24,6 @@ $(document).ready(function () {
 					.getElementsByTagName(
 						"input"
 					)[0].value = `${datarespose[k].site}`;
-
-				copyToClipboard(`${datarespose[k].site}`, k);
 			});
 		}
 	};
